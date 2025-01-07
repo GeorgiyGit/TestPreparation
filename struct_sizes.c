0c7f5ef95adca9d8e3b7b4619953bb4855d28890
George Sladkovsky <GeorgiyGitHub@gmail.com>
2025-01-07 17:04:40 +0100

    feat: add struct_sizes

---
+++ struct_sizes.c
@@ -0,0 +1,52 @@
+#include "stdio.h"
+
+typedef struct Test1{
+    char c;
+    int n;
+} Test1;
+
+typedef struct Test2{
+    char c;
+    char c2;
+    char c3;
+    int n;
+} Test2;
+
+typedef struct Test3{
+    char c;
+    char c2;
+    int n;
+    double d;
+} Test3;
+
+typedef struct Test4{
+    char c;
+    char c2;
+    int n;
+    long double d;
+} Test4;
+
+typedef struct Test5{
+    long double d;
+} Test5;
+
+typedef struct Test6{
+    char arr[32];
+} Test6;
+
+typedef struct Test7{
+    Test6 t;
+    int a;
+    char c;
+} Test7;
+
+
+int main(){
+    printf("Test1 - %lu\n",(unsigned long)sizeof(Test1));
+    printf("Test2 - %lu\n",(unsigned long)sizeof(Test2));
+    printf("Test3 - %lu\n",(unsigned long)sizeof(Test3));
+    printf("Test4 - %lu\n",(unsigned long)sizeof(Test4));
+    printf("Test5 - %lu\n",(unsigned long)sizeof(Test5));
+    printf("Test7 - %lu\n",(unsigned long)sizeof(Test7));
+    return 0;
+}
